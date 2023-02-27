a20a405964e0a98db7e626f5a1a9062bed957de0
Black854-notebook <juststepup854@gmail.com>
2023-02-27 20:04:39 +0600

    lvl 22 dialogs-NavLinks + Routes with *

--- social/src/components/Dialogs/Dialogs.jsx
+++ social/src/components/Dialogs/Dialogs.jsx
@@ -1,13 +1,22 @@
 import s from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
 
 const Dialogs = (props) => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <div className={s.dialog}>Карина</div>
-                <div className={s.dialog}>Сижик</div>
-                <div className={s.dialog}>Арсик</div>
-                <div className={s.dialog}>Систр</div>
+                <div className={s.dialog}>
+                    <NavLink to='1'>Карина</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='2'>Сижик</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='3'>Арсик</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='4'>Систр</NavLink>
+                </div>
             </div>
             <div className={s.messages}>
                 <div>Hi</div>
