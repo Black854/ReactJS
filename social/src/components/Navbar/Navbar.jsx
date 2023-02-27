84447e72a470a8b14849807fa8415f3a19c8539a
Black854 <juststepup854@gmail.com>
2023-02-27 17:51:13 +0600

    lvl 20 NavLink

--- social/src/components/Navbar/Navbar.jsx
+++ social/src/components/Navbar/Navbar.jsx
@@ -1,24 +1,26 @@
+import { NavLink } from 'react-router-dom';
 import s from './Navbar.module.css'
 
 const Navbar = () => {
   return (
     <nav className={s.nav}>
-      <div className={s.item}>
-        <a href='/profile'>Profile</a>
+      <div>
+        <NavLink className={({ isActive }) => isActive ? s.active : s.item} to='/profile'>Profile</NavLink>
       </div>
-      <div className={s.item}>
-        <a href='/dialogs'>Messages</a>
+      <div>
+        <NavLink className={({ isActive }) => isActive ? s.active : s.item} to='/dialogs'>Messages</NavLink>
       </div>
-      <div className={`${s.item} ${s.active}`}>
-        <nav>News</nav>
+      <div>
+        <NavLink className={({ isActive }) => isActive ? s.active : s.item} to='/news'>News</NavLink>
       </div>
-      <div className={s.item}>
-        <nav>Music</nav>
+      <div>
+        <NavLink className={({ isActive }) => isActive ? s.active : s.item} to='/music'>Music</NavLink>
       </div>
-      <div className={s.item}>
-        <nav>Settings</nav>
+      <div>
+        <NavLink className={({ isActive }) => isActive ? s.active : s.item} to='/settings'>Settings</NavLink>
       </div>
-    </nav>);
+    </nav>
+  );
 }
 
 export default Navbar
