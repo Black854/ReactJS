7a710f7045fccc3a1831163de53c17f72b652efb
Black854 <juststepup854@gmail.com>
2023-02-27 17:20:40 +0600

    lvl 19 BrowserRouter, Routes, Route

--- social/src/components/Navbar/Navbar.jsx
+++ social/src/components/Navbar/Navbar.jsx
@@ -3,20 +3,20 @@ import s from './Navbar.module.css'
 const Navbar = () => {
   return (
     <nav className={s.nav}>
+      <div className={s.item}>
+        <a href='/profile'>Profile</a>
+      </div>
+      <div className={s.item}>
+        <a href='/dialogs'>Messages</a>
+      </div>
       <div className={`${s.item} ${s.active}`}>
-        <a>Profile</a>
+        <nav>News</nav>
       </div>
       <div className={s.item}>
-        <a>Messages</a>
+        <nav>Music</nav>
       </div>
       <div className={s.item}>
-        <a>News</a>
-      </div>
-      <div className={s.item}>
-        <a>Music</a>
-      </div>
-      <div className={s.item}>
-        <a>Settings</a>
+        <nav>Settings</nav>
       </div>
     </nav>);
 }
