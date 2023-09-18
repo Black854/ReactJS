90cfc4bb958d0ea7f3675499d2a362e97b5c5e77
Black854-notebook <juststepup854@gmail.com>
2023-09-18 21:38:23 +0600

    lvl 89 - неудачная попытка тестов

---
+++ social/src/redux/profile-reducer.test.js
@@ -0,0 +1,17 @@
+import profileReducer, { createNewPost } from "./profile-reducer";
+
+it('new post should be added', () => {
+    let state = {
+        posts: [
+            {id: 1, postText: 'Hi bro', likesCount: 1},
+            {id: 2, postText: 'How are you?', likesCount: 0},
+            {id: 3, postText: 'Bro?', likesCount: 2}
+        ]
+    }
+
+    let action = createNewPost('hello');
+
+    let newState = profileReducer(state, action);
+
+    expect(newState.posts.length).toBe(4);
+})
